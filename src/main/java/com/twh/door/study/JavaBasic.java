74e9a7970c2f81a174044eff83948ea80c8403e8
EDZ <944714386@qq.com>
2024-04-17 11:28:06 +0800

    三根白发1.0

--- src/main/java/com/twh/door/study/JavaBasic.java
+++ src/main/java/com/twh/door/study/JavaBasic.java
@@ -5,11 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JavaBasic {
 
-    public void go(){
+    public static void go(){
 
         long a = Runtime.getRuntime().freeMemory();//剩余空间的字节数
         long b = Runtime.getRuntime().totalMemory();//方法总内存的字节数，
         long c = Runtime.getRuntime().maxMemory();//返回最大内存的字节数。
         log.info("a=:{}", a);
+    }
+
+    public static void main(String[] args) {
+        go();
     }
 }
