3d655334d8413a038c3128ec024daa757c4e580f
EDZ <944714386@qq.com>
2024-04-28 14:37:35 +0800

    三根白发1.0

---
+++ src/main/java/com/twh/door/utils/ClassConvertToMap.java
@@ -0,0 +1,24 @@
+package com.twh.door.utils;
+
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClassConvertToMap {
+
+
+    public Map<String, Object> objectToMap(Object obj) {
+        Map<String, Object> map = new HashMap<>();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            try {
+                map.put(field.getName(), field.get(obj));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return map;
+    }
+}
