82d297747b8ec9e9795205248e68f50b3d050b58
EDZ <944714386@qq.com>
2024-04-30 17:16:33 +0800

    三根白发1.0

--- src/main/java/com/twh/door/study/threadStudy/UseThreadPriority.java
+++ src/main/java/com/twh/door/study/threadStudy/UseThreadPriority.java
@@ -2,11 +2,10 @@ package com.twh.door.study.threadStudy;
 
 public class UseThreadPriority {
         public static void main(String[] args) {
-
             Thread t1 = new Thread("线程1") {
                 @Override
                 public void run() {
-                    for (int i = 1; i <= 50; i++) {
+                    for (int i = 1; i <= 10; i++) {
                         System.out.println(getName() + "----" + i);
                     }
                 }
@@ -14,7 +13,7 @@ public class UseThreadPriority {
             Thread t2 = new Thread("线程2") {
                 @Override
                 public void run() {
-                    for (int i = 1; i <= 50; i++) {
+                    for (int i = 1; i <= 10; i++) {
                         System.out.println(getName() + "----" + i);
                     }
                 }
@@ -22,9 +21,9 @@ public class UseThreadPriority {
             // 获取线程对象默认优先级
             System.out.println(t1.getPriority()); // 5
             System.out.println(t2.getPriority()); // 5
-        /* // 练习:获取主线程优先级
-        Thread t = Thread.currentThread();
-        System.out.println(t.getName() + "----" + t.getPriority()); // main----5*/
+            /* // 练习:获取主线程优先级
+            Thread t = Thread.currentThread();
+            System.out.println(t.getName() + "----" + t.getPriority()); // main----5*/
             // 设置线程优先级 数字必须是[1, 10] 超出范围就会报错
             t2.setPriority(Thread.MAX_PRIORITY);
             t1.setPriority(Thread.MIN_PRIORITY);
